a25ea4505641b3253fd96f53b7a6cfbc1b4b42c4
Edneymesquita <edneyphp@outlook.com>
2020-09-25 21:05:33 -0300

    Basic API Application

---
+++ Commander/Data/MockCommanderRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepository : ICommanderRepository
+    {
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { Id = 1, HowTo = "Boild an egg", Line = "Boil Water", Platform = "Kettle & Pan" },
+                new Command { Id = 2, HowTo = "Cut bread", Line = "Get a knife", Platform = "Knife & chopping board" },
+                new Command { Id = 3, HowTo = "Make cup of tea", Line = "Place teabag in cup", Platform = "Kettle & Cut" }
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 1, HowTo = "Boild an egg", Line = "Boil Water", Platform = "Kettle & Pan" };
+        }
+    }
+}
